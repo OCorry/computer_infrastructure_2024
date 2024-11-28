4e44ddabca165a5a28e85b167751e3209992ca52
Orla Corry <98124862+OCorry@users.noreply.github.com>
2024-11-28 22:04:54 +0000

    downloaded today's Athenry weather data and started Task 9: Pandas

--- weather.sh
+++ weather.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 echo "Download of weather data started at:" $(date)
-wget -O data/weather/`date +"%Y%m%d_%H%M%S.json"` https://prodapi.metweb.ie/observations/athenry/today
+wget -O data/weather/`date +"%Y%m%d_%H%M%S_athenry.json"` https://prodapi.metweb.ie/observations/athenry/today
 echo "Download of weather data completed at:" $(date)

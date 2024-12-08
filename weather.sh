b8d62e0f62f00025e0c04c564dd0cabc0515c34b
Orla Corry <98124862+OCorry@users.noreply.github.com>
2024-12-08 17:51:05 +0000

    Amended Syntax in weather.sh script to try resolve error in GitHub Actions

--- weather.sh
+++ weather.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-
-echo "Download of weather data started at:" $(date)
 wget -O data/weather/`date +"%Y%m%d_%H%M%S_athenry.json"` https://prodapi.metweb.ie/observations/athenry/today
-echo "Download of weather data completed at:" $(date)
+

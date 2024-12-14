1a55220b55a7206af4bcd7e4677a52576e6ae05c
Orla Corry <98124862+OCorry@users.noreply.github.com>
2024-12-14 21:21:23 +0000

    Tidied up write up of task 6 & 7 in notebook. removed sync_with_repo file. Updated requirements.txt

--- sync_with_repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# sync_with_repo.sh: Sync changes from the repository to Codespaces
-echo "Pulling latest changes into Codespaces..."
-git fetch origin
-git pull origin main --rebase
-echo "Sync complete!"

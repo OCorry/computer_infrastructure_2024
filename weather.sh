eafbe03c293fe9fcf6099510ac2613a8041b750a
Orla Corry <98124862+OCorry@users.noreply.github.com>
2024-12-17 22:57:39 +0000

    Finished tidying notebook & references. added alternative option to weather.sh & moved directory_hierarchy.md to img folder

--- weather.sh
+++ weather.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+# wget command to download weather data using APi and store in data/weather directory as a timestamped file
 wget -O data/weather/`date +"%Y%m%d_%H%M%S_athenry.json"` https://prodapi.metweb.ie/observations/athenry/today
 
+# Alternative to wget command
+# curl -o data/weather/`date +"%Y%m%d_%H%M%S_athenry.json"` https://prodapi.metweb.ie/observations/athenry/today
